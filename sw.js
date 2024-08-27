93fba88ed2e303f8e63f26f143abf3c46143b9cb
ADMINISTRAT0R-KING <148674689+ADMINISTRAT0R-KING@users.noreply.github.com>
2024-08-27 18:23:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
     const ToCacheFileList=[
     "./",
-    "./index.html",
-    "./style.css",
-    "./main.js",
-    "./sw.js",
-    "/ASSETS/0.png",
-    "/ASSETS/0.ttf",
-    "/ASSETS/0.jpeg",
-    "/ASSETS/1.jpeg",
-    "/ASSETS/2.jpeg",
-    "/ASSETS/3.jpeg",
-    "/ASSETS/4.jpeg",
+    "/Clock/index.html",
+    "/Clock/style.css",
+    "/Clock/main.js",
+    "/Clock/sw.js",
+    "/Clock/ASSETS/0.png",
+    "/Clock/ASSETS/0.ttf",
+    "/Clock/ASSETS/0.jpeg",
+    "/Clock/ASSETS/1.jpeg",
+    "/Clock/ASSETS/2.jpeg",
+    "/Vlock/ASSETS/3.jpeg",
+    "/Clock/ASSETS/4.jpeg",
   ],CacheName='V2';
   
   
