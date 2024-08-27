a609ee85c1f76d7253a7c3e2b7ef7a360f555c0d
ADMINISTRAT0R-KING <148674689+ADMINISTRAT0R-KING@users.noreply.github.com>
2024-08-27 18:29:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
     const ToCacheFileList=[
     "./",
-    "/Clock/index.html",
-    "/Clock/style.css",
-    "/Clock/main.js",
-    "/Clock/sw.js",
-    "/Clock/ASSETS/0.png",
-    "/Clock/ASSETS/0.ttf",
-    "/Clock/ASSETS/0.jpeg",
-    "/Clock/ASSETS/1.jpeg",
-    "/Clock/ASSETS/2.jpeg",
-    "/Vlock/ASSETS/3.jpeg",
-    "/Clock/ASSETS/4.jpeg",
-  ],CacheName='V2';
+    "./index.html",
+    "./style.css",
+    "./main.js",
+    "./sw.js",
+    "./ASSETS/0.png",
+    "./ASSETS/0.ttf",
+    "./ASSETS/0.jpeg",
+    "./ASSETS/1.jpeg",
+    "./ASSETS/2.jpeg",
+    "./ASSETS/3.jpeg",
+    "./ASSETS/4.jpeg",
+  ],CacheName='V1';
   
   
   self.addEventListener('install',(event)=>{
